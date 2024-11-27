7a4dcf23122d27f17d5bb5ccbbb676924bc44c6b
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2024-11-27 22:18:55 +0200

    Booking

--- Teen-Konnekt-App/lib/screens/appointment_info_page.dart
+++ Teen-Konnekt-App/lib/screens/appointment_info_page.dart
@@ -1,1 +1,196 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'slot.dart';
 
+class AppointmentInfoPage extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController sexController = TextEditingController();
+  final TextEditingController yearController1 = TextEditingController();
+  final TextEditingController yearController2 = TextEditingController();
+  final TextEditingController yearController3 = TextEditingController();
+  final TextEditingController yearController4 = TextEditingController();
+  final TextEditingController problemController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.green),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _buildTextField("Full Names", nameController, TextInputType.name),
+            SizedBox(height: 16),
+            _buildTextField("Sex", sexController, TextInputType.text),
+            SizedBox(height: 16),
+            Text(
+              "Year of Birth",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _buildYearField(yearController1),
+                _buildYearField(yearController2),
+                _buildYearField(yearController3),
+                _buildYearField(yearController4),
+              ],
+            ),
+            SizedBox(height: 16),
+            _buildMultilineTextField("Problem Description", problemController),
+            SizedBox(height: 32),
+            Center(
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 14),
+                  ),
+                  onPressed: () {
+                    String year = yearController1.text +
+                        yearController2.text +
+                        yearController3.text +
+                        yearController4.text;
+
+                    if (year.length == 4 &&
+                        int.tryParse(year) != null &&
+                        int.parse(year) > 1900 &&
+                        int.parse(year) <= DateTime.now().year) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SlotBookingPage(
+                            name: nameController.text,
+                            sex: sexController.text,
+                            yearOfBirth: year,
+                            problem: problemController.text,
+                          ),
+                        ),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Please enter a valid year.')),
+                      );
+                    }
+                  },
+                  child: Text(
+                    "Next",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(
+      String label, TextEditingController controller, TextInputType type) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+          ),
+        ),
+        SizedBox(height: 8),
+        TextField(
+          controller: controller,
+          keyboardType: type,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildYearField(TextEditingController controller) {
+    return SizedBox(
+      width: 50,
+      child: TextField(
+        controller: controller,
+        keyboardType: TextInputType.number,
+        maxLength: 1,
+        textAlign: TextAlign.center,
+        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+        decoration: InputDecoration(
+          counterText: "", // Removes the counter text (e.g., "0/1")
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          contentPadding: EdgeInsets.symmetric(vertical: 12),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMultilineTextField(
+      String label, TextEditingController controller) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+          ),
+        ),
+        SizedBox(height: 8),
+        TextField(
+          controller: controller,
+          maxLines: 5,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          ),
+        ),
+      ],
+    );
+  }
+}
